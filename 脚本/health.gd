076374c6166d437cff85cc5e468a561d9d9ee6aa
sean <sean@seandepp.com>
2026-03-25 13:54:08 +0800

    血条文件清除

--- 脚本/health.gd
+++
@@ -1,9 +0,0 @@
-extends TextureRect
-
-@onready var full_heart = $Full
-@onready var empty_heart = $Empty
-
-# 设置爱心状态
-func set_heart_state(is_full: bool) -> void:
-	full_heart.visible = is_full
-	empty_heart.visible = !is_full
